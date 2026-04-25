8590896c8f4554f6530f9323cb705a2c6777408a
piperal <piperal@tlu.ee>
2026-04-25 21:29:05 +0300

    added audio and updated ui

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ class Typer {
         this.name = "";
         this.correctLetter = 0
         this.incorrectLetter = 0
-        this.wordsCounter = 6;
+        this.activeAudio = document.getElementById("activeAudio")
+        this.winAudioGood = document.getElementById("winGood")
+        this.wordsCounter = 5;
         this.wordLength = 2;
         this.startTime = 0;
         this.endTime = 0;
@@ -47,6 +49,9 @@ class Typer {
                 this.words[wordLength].push(word)
             }
         }
+        this.activeAudio.volume = 0.2
+        this.activeAudio.play()
+
         this.startGame()
     }
 
@@ -82,7 +87,7 @@ class Typer {
         }
         else if (this.word[0] === event && this.word.length == 1 && this.typeWords.length - 2 == this.wordsType) {
             this.wordsType = 0
-            document.getElementById("word").innerHTML = "Game over"
+            console.log("Game over")
         }
         else if (this.word[0] != event) {
             this.incorrectLetter += 1
@@ -116,10 +121,14 @@ class Typer {
 
     enterName() {
         console.log("Correct leters:", this.correctLetter, "Incorrect Letters:", this.incorrectLetter)
+        this.activeAudio.pause()
+        this.winAudioGood.volume = 0.2
+        this.winAudioGood.play()
         let nameContainer = document.getElementById("insertNameContainer")
         let nameInput = document.getElementById("nameInput")
         let saveName = document.getElementById("saveName")
         nameContainer.style.visibility = "visible"
+        document.getElementById("word").style.visibility = "hidden"
         saveName.addEventListener("click", () => {
             console.log(nameInput.value)
             if (nameInput.value === "") {
@@ -135,7 +144,7 @@ class Typer {
                 this.name = nameInput.value
                 document.getElementById("word").style.visibility = "hidden"
                 this.saveResult()
-
+                location.reload()
             }
         })
 
@@ -145,28 +154,39 @@ class Typer {
         document.getElementById("word").innerHTML = this.word
     }
 
-    async saveResult() {
+    scoreCalc() { }
+
+    saveResult() {
         let result = {
             name: this.name,
-            time: this.score
+            score: this.correctLetter - this.incorrectLetter,
+            time: this.timer
         }
-        localStorage.setItem("score", JSON.stringify(result))
-
-        try {
-            await fetch("server.php", {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: "save=" + encodeURIComponent(JSON.stringify(this.result))
-            });
-            console.log("Success!")
-        }
-        catch (err) {
-            console.log(err)
-        }
+        console.log(result)
+        var olddata = JSON.parse(localStorage.getItem("score")) || []
+        olddata.push(result)
+        localStorage.setItem("score", JSON.stringify(olddata))
     }
 
 
 }
+
+//Leaderboards controller
+let clicked = false
+document.getElementById("leaderboardsBtn").addEventListener("click", () => {
+    console.log(clicked)
+    if (!clicked) {
+        document.getElementById('leaderboardContainer').style.left = "0%"
+        clicked = true
+    }
+    else if (clicked) {
+        document.getElementById('leaderboardContainer').style.left = "-100%"
+        clicked = false
+    }
+})
+
+
+//Start game controller
 document.getElementById("startGame").addEventListener("click", () => {
     let infoCont = document.getElementById("info")
     let wordCont = document.getElementById("wordContainer")
@@ -175,6 +195,11 @@ document.getElementById("startGame").addEventListener("click", () => {
     document.getElementById("info").style.visibility = "visible"
     document.getElementById("name").style.display = "none"
     let count = 3;
+    let startAudio = document.getElementById("myAudio")
+    setTimeout(() => {
+        startAudio.volume = 0.2
+        startAudio.play()
+    }, 500)
     const countdown = setInterval(() => {
         document.getElementById("word").style.visibility = "visible"
         document.getElementById("word").innerHTML = count
@@ -184,10 +209,45 @@ document.getElementById("startGame").addEventListener("click", () => {
             clearInterval(countdown);
             wordCont.style.visibility = "visible"
             infoCont.style.visibility = "visible"
-            let typer = new Typer();
+            new Typer();
         }
     }, 1000);
     countdown()
 
 })
+function updateLeaderboard() {
+    let users = JSON.parse(localStorage.getItem("score")) || []
+    console.log(users[0])
+    if (users[0] === undefined) {
+        document.getElementById("leaderboardContainer").innerHTML += `<h2>No scores have been submitted</h2>`
+    }
+    users.sort(function (a, b) {
+        return b.score - a.score
+    })
+    for (let i = 0; i < users.length; i++) {
+        document.getElementById("leaderboardContainer").innerHTML += `<h1>${users[i].name}, Score: ${users[i].score}</h1>`
+    }
+}
 
+function fontChanger() {
+    const fonts = [
+        "Arial",
+        "Helvetica",
+        "Verdana",
+        "Times New Roman",
+        "Tahoma",
+        "Trebuchet MS",
+        "Archivo Black",
+        "Lobster Two"
+    ];
+
+    let index = 0;
+    const text = document.getElementById("title");
+
+    setInterval(() => {
+        text.style.fontFamily = fonts[index];
+        index = Math.floor(Math.random() * fonts.length);
+    }, 200);
+}
+
+window.addEventListener("load", () => {updateLeaderboard(); fontChanger()})
